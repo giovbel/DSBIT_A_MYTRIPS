33a021554d8ece06129d8eea1d09d1a629a75032
GIOVANNA SILVA BELO <23111786@dsn127m1246727.cfp127aluno.local>
2024-03-12 11:14:13 -0300

    finalizado

--- app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
@@ -23,6 +23,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.sharp.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -36,9 +38,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -267,17 +273,253 @@ fun MyTrips() {
                 color = Color(0xFFA09C9C)
             )
         }
-        Card (
-            modifier = Modifier
-                .height(30.dp)
-                .width(30.dp),
-            colors = CardDefaults
-                .cardColors(containerColor = Color(0xFFF6F6F6))
-        ){
 
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Card(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp),
+                shape = RoundedCornerShape(50.dp),
+                colors = CardDefaults
+                    .cardColors(containerColor = Color(0xFFF6F6F6)),
+                border = BorderStroke(
+                    2.dp,
+                    Brush.horizontalGradient(listOf(Color(0xFFCF06F0), Color.White))
+                )
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pessoa),
+                    contentDescription = "perfil foto",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .padding(top = 16.dp),
+                    alignment = Alignment.Center
+                )
+            }
+        }
+        Image(
+            modifier = Modifier
+                .size(30.dp)
+                .offset(x = 220.dp, y = -25.dp),
+            painter = painterResource(id = R.drawable.camera),
+            contentDescription = "câmera"
+        )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                        modifier = Modifier
+                            .width(350.dp)
+                            .padding(bottom = 18.dp),
+                value = "Susanna Hoffs",
+                onValueChange = {},
+                label = {
+                    Text(
+                        text = "Username",
+                        color = Color(0xFF000000),
+                        fontFamily = Poppins
+                    )
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        unfocusedBorderColor = Color(0xFFCF06F0),
+                        unfocusedContainerColor = Color(0xFFFFFFFF),
+                        focusedBorderColor = Color(0xFFCF06F0),
+                        unfocusedLeadingIconColor = Color(0xFFCF06F0)
+                    ),
+                shape = RoundedCornerShape(12.dp),
+                leadingIcon = {
+                    Icon(
+                        Icons.Filled.Person,
+                        contentDescription = "icone pessoa",
+                        modifier = Modifier.size(30.dp),
+                    )
+                }
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .width(350.dp)
+                        .padding(bottom = 18.dp),
+                    value = "99999-0987",
+                    onValueChange = {},
+                    label = {
+                        Text(
+                            text = "Phone",
+                            color = Color(0xFF000000),
+                            fontFamily = Poppins
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults
+                        .colors(
+                            unfocusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedContainerColor = Color(0xFFFFFFFF),
+                            focusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedLeadingIconColor = Color(0xFFCF06F0)
+                        ),
+                    shape = RoundedCornerShape(12.dp),
+                    leadingIcon = {
+                        Image(
+                            painter = painterResource(id = R.drawable.phone),
+                            contentDescription = "telefone",
+                            modifier = Modifier
+                                .size(30.dp)
+                        )
+                    }
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .width(350.dp)
+                        .padding(bottom = 18.dp),
+                    value = "susanna@email.com ",
+                    onValueChange = {},
+                    label = {
+                        Text(
+                            text = "E-mail",
+                            color = Color(0xFF000000),
+                            fontFamily = Poppins
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults
+                        .colors(
+                            unfocusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedContainerColor = Color(0xFFFFFFFF),
+                            focusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedLeadingIconColor = Color(0xFFCF06F0)
+                        ),
+                    shape = RoundedCornerShape(12.dp),
+                    leadingIcon = {
+                        Icon(
+                            Icons.Filled.Email,
+                            contentDescription = "icone email",
+                            modifier = Modifier.size(30.dp),
+                        )
+                    }
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .width(350.dp)
+                        .padding(bottom = 18.dp),
+                    value = "************",
+                    onValueChange = {},
+                    label = {
+                        Text(
+                            text = "Password",
+                            color = Color(0xFF000000),
+                            fontFamily = Poppins
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults
+                        .colors(
+                            unfocusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedContainerColor = Color(0xFFFFFFFF),
+                            focusedBorderColor = Color(0xFFCF06F0),
+                            unfocusedLeadingIconColor = Color(0xFFCF06F0)
+                        ),
+                    shape = RoundedCornerShape(12.dp),
+                    leadingIcon = {
+                        Icon(
+                            Icons.Filled.Lock,
+                            contentDescription = "icone senha",
+                            modifier = Modifier.size(30.dp),
+                        )
+                    }
+                )
+            }
+
+           Row (
+               modifier = Modifier
+                   .padding(start = 25.dp),
+           ) {
+               Card(
+                   modifier = Modifier
+                       .height(20.dp)
+                       .width(20.dp),
+                   shape = RectangleShape,
+                   colors = CardDefaults
+                       .cardColors(containerColor = Color(0xFF)),
+                   border = BorderStroke(1.dp, color = Color(0xFFCF06F0))
+               )
+               {
+               }
+               Text(
+                   modifier = Modifier
+                       .padding(start = 10.dp),
+                   text = "Over 18?"
+               )
+           }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(start = 12.dp, end = 12.dp, top = 15.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFCF06F0)
+            ),
+            shape = RoundedCornerShape(10.dp)
+
+        ) {
+            Text(
+                text = "CREATE ACCOUNT",
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = Poppins
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .offset(130.dp)
+                .padding(top = 12.dp)
+        ) {
+            Text(
+                text = "Already have an account?",
+                fontSize = 14.sp,
+                color = Color(0xFFA09C9C),
+                fontFamily = Poppins
+            )
+            Text(
+                text = " Sign in",
+                fontSize = 14.sp,
+                color = Color(0xFFCF06F0),
+                fontWeight = FontWeight.Bold,
+                fontFamily = Poppins
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(70.dp)
+                    .offset(y = 25.dp)
+                    .offset(x = -40.dp),
+                colors = CardDefaults
+                    .cardColors(containerColor = Color(0xFFCF06F0))
+            ){
+
+            }
+        }
         }
     }
-}
+
     @Preview(showBackground = true, showSystemUi = true)
     @Composable
     fun GreetingPreview() {
